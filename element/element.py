9c9cbe5aaec7f405a3f5843104f4c705fc1901e4
Grey <greytrace@naver.com>
2024-12-13 16:50:49 +0900

    오타 수정

--- element/element.py
+++ element/element.py
@@ -8,7 +8,7 @@ lotto_ok2 = {'Target':'input#btnBuy'}
 lotto_ok3 = {'Target':'div.layer-alert#popupLayerConfirm input.button.lrg.confirm'}
 
 lp72_jogroup = {'Target':"span.jogroup"}
-lp72_allgroup = {'Target:':'span.jogroup.all'}
+lp72_allgroup = {'Target':'span.jogroup.all'}
 lp72_nums = {'Target':'span#SNUM'}
 lp72_ok1 = {'Target':'a.lotto720_btn_confirm_number'}
 lp72_ok2 = {'Target':'a.lotto720_btn_pay'}
